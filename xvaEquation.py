0ca4a9e05ad55d633267fad6e85e044205d01912
Chang Jiang <cjiang8613@foxmail.com>
2020-05-11 16:36:33 +0100

    fix driver function

--- xvaEquation.py
+++ xvaEquation.py
@@ -9,14 +9,11 @@ class PricingForward(Equation):
         self.strike = eqn_config.strike
         self.x_init = np.ones(self.dim) * eqn_config.x_init  # initial value of x, the underlying
         self.sigma = eqn_config.sigma    # volatility 
-        self.mu_bar = eqn_config.r    # drift 
-        self.rl = 0.0        # lending rate
-        self.rb = 0.0        # borrowing rate      
+        self.rate = eqn_config.r    # drift     
         self.useExplict = False #whether to use explict formula to evaluate dyanamics of x
 
     
-    def sample(self, num_sample):
-        
+    def sample(self, num_sample):       
         
         dw_sample = normal.rvs(size=[num_sample,     
                                      self.dim,
@@ -28,21 +25,19 @@ class PricingForward(Equation):
         x_sample[:, :, 0] = np.ones([num_sample, self.dim]) * self.x_init  
 
         if self.useExplict: #use analytic solution of linear SDE
-            factor = np.exp((self.mu_bar-(self.sigma**2)/2)*self.delta_t)
+            factor = np.exp((self.rate-(self.sigma**2)/2)*self.delta_t)
             for i in range(self.num_time_interval):   
                 x_sample[:, :, i + 1] = (factor * np.exp(self.sigma * dw_sample[:, :, i])) * x_sample[:, :, i]
         else:   #use Euler-Maruyama scheme
             for i in range(self.num_time_interval):
-         	    x_sample[:, :, i + 1] = (1 + self.mu_bar * self.delta_t) * x_sample[:, :, i] + (self.sigma * x_sample[:, :, i] * dw_sample[:, :, i])           
+         	    x_sample[:, :, i + 1] = (1 + self.rate * self.delta_t) * x_sample[:, :, i] + (self.sigma * x_sample[:, :, i] * dw_sample[:, :, i])           
       
         
         return dw_sample, x_sample   
 
   
     def f_tf(self, t, x, y, z):
-        
-        temp = tf.reduce_sum(z, 1, keepdims=True) / self.sigma 
-        return -self.rl * y - (self.mu_bar - self.rl) * temp  # the driver is  -(mu/sigma)* Z
+        return -self.rate*y
    
     def g_tf(self, t, x):
         return x - self.strike

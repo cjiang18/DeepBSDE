ff7efdb8cef327acc61dd729ebf1868ac25b42f6
Chang Jiang <cjiang8613@foxmail.com>
2020-05-14 14:46:26 +0100

    XVA evaluation implemented

--- xvaEquation.py
+++ xvaEquation.py
@@ -5,7 +5,8 @@ from scipy.stats import multivariate_normal as normal
 from tqdm import tqdm
 
 class EuropeanEquation(Equation):
-    # abstract class for European type xVA evaluation
+    # abstract class for European type xVA evaluation. 
+    # Function g_tf has to be implemented
     def __init__(self,eqn_config):
         super(EuropeanEquation,self).__init__(eqn_config)
         self.strike = eqn_config.strike
@@ -60,9 +61,9 @@ class EuropeanEquation(Equation):
         return mean, [mean-3*std,mean+3*std]
 
 
-class PricingForward(EuropeanEquation):
+class ForwardContract(EuropeanEquation):
     def __init__(self,eqn_config):
-        super(PricingForward, self).__init__(eqn_config)     
+        super(ForwardContract, self).__init__(eqn_config)     
   
 
     def g_tf(self, t, x):
@@ -73,11 +74,48 @@ class PricingForward(EuropeanEquation):
 class BasketOption(EuropeanEquation):
 
     def __init__(self, eqn_config):
-        super(BasketOption, self).__init__(eqn_config)
-      
+        super(BasketOption, self).__init__(eqn_config)     
 
 
     def g_tf(self, t, x):
-
         temp = tf.reduce_sum(x, 1,keepdims=True)
         return tf.maximum(temp - self.strike, 0)
+
+
+class XVA(Equation):
+    def __init__(self,eqn_config,clean_value):
+        super(XVA,self).__init__(eqn_config)
+        self.rate = eqn_config.r # risk-free return
+        self.intensityB = eqn_config.intensityB # default intensity of the bank
+        self.intensityC = eqn_config.intensityC # default intensity of the conterparty
+        self.r_fl = eqn_config.r_fl # unsecured funding lending rate
+        self.r_fb = eqn_config.r_fb # unsecured funding borrowing rate
+        self.r_cl = eqn_config.r_cl # interest rate on posted collateral
+        self.r_cb = eqn_config.r_cb # interest rate on received collateral
+        self.clean_value = clean_value  # a tf.keras.model object (trained neural network), respresenting the clean value process
+        self.collateral = eqn_config.collateral
+
+        #setting (default) value for recovery rate
+        try:
+            self.R_B = eqn_config.Recovery_Bank
+        except AttributeError:
+            self.R_B = 0.6
+        try:
+            self.R_C = eqn_config.Recovery_Counterparty
+        except AttributeError:
+            self.R_C = 0.6
+
+    def sample(self, num_sample):
+        return self.clean_value.predict(num_sample)
+
+    def f_tf(self, t, x, y, z):
+        cva = (1-self.R_C)*tf.maximum(self.collateral-x,0)*self.intensityC
+        dva = (1-self.R_B)*tf.maximum(x-self.collateral,0)*self.intensityB
+        fva = (self.r_fl-self.rate)*tf.maximum(x-y-self.collateral,0) - (self.r_fb-self.rate)*tf.maximum(self.collateral+y-x,0)
+        colva = (self.r_cl-self.rate)*max(self.collateral,0) - (self.r_cb-self.rate)*max(-self.collateral,0)
+        discount = -(self.rate+self.intensityB+self.intensityC)*y
+
+        return -cva + dva + fva + colva + discount
+    
+    def g_tf(self, t, x):
+        return 0.
